561a3334281f4efc6002b7c26dbead3861b3eb33
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-08-05 18:20:18 +0200

    code during call

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -2,17 +2,16 @@
     <!-- Компонент списка VListItem от Vuetify -->
     <v-list-item>
       <!-- Содержимое элемента списка VListItemContent -->
-      <v-list-item-content>
         <!-- Заголовок элемента списка VListItemTitle -->
         <v-list-item-title>{{ character.name }}</v-list-item-title>
         <!-- Подзаголовок с ростом персонажа VListItemSubtitle -->
         <v-list-item-subtitle>Height: {{ character.height }}</v-list-item-subtitle>
         <!-- Подзаголовок с массой персонажа VListItemSubtitle -->
         <v-list-item-subtitle>Mass: {{ character.mass }}</v-list-item-subtitle>
-      </v-list-item-content>
+        <button @click="onLike">Like</button>
     </v-list-item>
   </template>
-  
+
   <script>
   export default {
     // Название компонента
@@ -23,8 +22,24 @@
       character: {
         type: Object,
         required: true
+      },
+      onLike: {
+        // type: Function ??
+        required: true
+      }
+    },
+    data(){
+      return {
+        isLiked_2: false // todo: тогда надо хранить это в локалстораж
+      }
+    },
+    methods:{
+      onLike_3(){
+        characters[5].isLiked = true //todo: this is impossible!!!
+      },
+      onLike_2() {
+        this.isLiked = true
       }
     }
   }
   </script>
-  
--- src/components/StarCharList.vue
+++ src/components/StarCharList.vue
@@ -10,112 +10,99 @@
         <!-- Проход по каждому персонажу в currentCharacters и отображение его данных через CharacterCard -->
         <CharacterCard v-for="(char, index) in currentCharacters" :key="index" :character="char" />
       </v-list>
-     
+
     </div>
     <!-- Кнопка для перехода на предыдущую страницу -->
-    <v-btn @click="previousPage" :disabled="isButtonDisabledPrev">Previous</v-btn>
+    <v-btn @click="previousPage" :disabled="isPrevButtonDisabled">Previous</v-btn>
     <!-- Кнопка для перехода на следующую страницу -->
-    <v-btn @click="nextPage" :disabled="isButtonDisabledNext">Next</v-btn>
+    <v-btn @click="nextPage" :disabled="isNextButtonDisabled">Next</v-btn>
   </v-container>
 </template>
 
 <script>
+const CHARACTERS_PER_PAGE = 6;
 import CharacterCard from './CharacterCard.vue'
+import { characterMap } from "@/mapping.js";
 
 export default {
   components: {
-    CharacterCard 
+    CharacterCard
   },
   data() {
     return {
       isLoading: false,   // Инициализация состояния загрузки как false
       error: '',          // Инициализация сообщения об ошибке как пустая строка
       currentPage: 1,     // Инициализация текущей страницы как 1
-      characters: [],     // Инициализация массива персонажей как пустого
-      isButtonDisabledNext: false,
-      isButtonDisabledPrev: false
+      characters: [],     // Инициализация массива персонажей как пустого,
     }
   },
 
   async mounted() {
     // Загружаем персонажей для первой страницы при монтировании компонента
     this.characters = await this.fetchCharacters(1)
-   // Вызов метода для отключения кнопки "предыдущая"
-    
+    // Вызов метода для отключения кнопки "предыдущая"
   },
-  
+
   computed: {
-    
     // Вычисляемые данные для текущих персонажей на основе текущей страницы
     currentCharacters() {
       const startIdx = (this.currentPage - 1) * 10 // Начальный индекс для текущей страницы
       console.log(this.currentPage)
-      this.disablePrevButton()
       return this.characters.slice(startIdx, startIdx + 10) // Возвращаем 10 персонажей
-      
+    },
+    isNextButtonDisabled() {
+      return this.currentPage > 8
+    },
+    isPrevButtonDisabled() {
+      return this.currentPage <= 1
     }
   },
- 
- watch: {
-  currentPage() {
-  this.disablePrevButton()
-  this.disableNextButton()
-  },
-
- },
 
   methods: {
     // Метод для получения персонажей с определенной страницы
     fetchCharacters(page) {
-      this.isLoading = true; // Устанавливаем состояние загрузки в true
+      // this.isLoading = true; // Устанавливаем состояние загрузки в true
       return fetch(`https://swapi.dev/api/people/?page=${page}&format=json`) // Выполняем запрос к API
-        .then(response => response.json()) // Парсим ответ как JSON
-        .then(data => {
-          return data.results // Возвращаем результаты запроса
-        })
-        .catch((e) => {
-          console.error("Произошла ошибка", e); // Логируем ошибку в консоль
-          this.error = "Серверная ошибка"; // Устанавливаем сообщение об ошибке
-        })
-        .finally(() => {
-          this.isLoading = false; // Сбрасываем состояние загрузки
-        });
+      .then(response => response.json()) // Парсим ответ как JSON
+      .then(data => {
+        return data.results.map(characterMap) // Возвращаем результаты запроса
+      })
+      .catch((e) => {
+        console.error("Произошла ошибка", e); // Логируем ошибку в консоль
+        this.error = "Серверная ошибка"; // Устанавливаем сообщение об ошибке
+      })
+      .finally(() => {
+        this.isLoading = false; // Сбрасываем состояние загрузки
+      });
+    },
+    onLike(id){
+      const char = this.characters.find(id)
+      char.isLiked = true
+      this.characters = ...
     },
     // Метод для перехода на следующую страницу
     async nextPage() {
       if (this.characters.length !== this.currentPage * 10) {
         // Если количество персонажей не соответствует текущей странице, увеличиваем страницу и выходим из функции
         this.currentPage++
-       
+        // fixme: этот код не достаточно "общий", он работает только с 10 персонажами на странице.
+        // fixme: даже только в том случае, когда ответ сервера и интерфейс клиента - совпадают
         return
       }
-     
-      this.currentPage++ // Увеличиваем текущую страницу
-      const newCharacters = await this.fetchCharacters(this.currentPage) // Получаем новых персонажей для новой страницы
+
+      const newCharacters = await this.fetchCharacters(this.currentPage + 1) // Получаем новых персонажей для новой страницы
       this.characters = [...this.characters, ...newCharacters] // Обновляем массив персонажей новыми данными
-      
+      this.currentPage++ // Увеличиваем текущую страницу
     },
     // Метод для перехода на предыдущую страницу
     previousPage() {
       if (this.currentPage === 1) {
-      return // Если текущая страница первая, выходим из функции
+        return // Если текущая страница первая, выходим из функции
       }
       this.currentPage-- // Уменьшаем текущую страницу
       console.log('previousPage', this.currentPage) // Логируем номер предыдущей страницы
     },
-    disablePrevButton () {
-     if (this.currentPage === 1) {
-        this.isButtonDisabledPrev = true;
-     }
-     else {this.isButtonDisabledPrev = false;}
-    },
-    disableNextButton () {
-      if (this.currentPage >8) {
-        this.isButtonDisabledNext = true;
-     }
-     else {this.isButtonDisabledNext = false;}
-    }
-    }
-  }
+  },
+}
 
 </script>
