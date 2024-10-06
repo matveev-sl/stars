90cbc0d4cb7116dfb996d474ed5654327b671217
Vladimir Kromm <vkromm@gmail.com>
2024-10-06 09:59:52 +0200

    ревью

--- theory/Homework_15/finobonachi.js
+++ theory/Homework_15/finobonachi.js
@@ -5,3 +5,21 @@ function fibonacci(n) {
   if (n === 1) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+const cache = {};
+
+function fibonacci2(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (cache[n]) return cache[n];
+  cache[n] = fibonacci2(n - 1) + fibonacci2(n - 2);
+  return cache[n];
+}
+
+const now = Date.now();
+console.log(fibonacci(40));
+console.log('Execution1', Date.now() - now);
+
+const now1 = Date.now();
+console.log(fibonacci2(40));
+console.log('Execution2', Date.now() - now1);
