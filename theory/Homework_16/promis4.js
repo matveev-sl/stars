85ee604dd9602f0bcf6951ed5459a1b8a37fc0b9
matveev-sl <48273254+matveev-sl@users.noreply.github.com>
2024-10-16 20:26:11 +0200

    right commit

--- theory/Homework_16/promis4.js
+++ theory/Homework_16/promis4.js
@@ -1,24 +1,28 @@
 function randomStringWithChance(successRate = 0.7) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        Math.random() < successRate ? resolve(Math.random().toString(36).substring(2, 8)) : reject('Ошибка');
-      }, 1000);
-    });
+  const randomDelay = Math.floor(Math.random() * (1500 - 500 + 1)) + 500; 
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      Math.random() < successRate
+        ? resolve(Math.random().toString(36).substring(2, 8))
+        : reject('Ошибка');
+    }, randomDelay);
+  });
+}
+
+const getRandomStringWithRetries = async (maxPromisesCount = 5, chance = 0.5) => {
+  const promises = Array.from({ length: maxPromisesCount }, () => randomStringWithChance(chance));
+  try {
+    return await Promise.any(promises);
+  } catch (error) {
+    throw new Error('Не удалось получить строку');
   }
-  
-  const getRandomStringWithRetries = async (maxRetriesCount = 20, chance = 0.5) => {
-    const promises = Array.from({ length: maxRetriesCount}, () => randomStringWithChance(chance));
-    try {
-        return await Promise.any(promises); 
-      } catch (error) {
-        throw new Error('Не удалось получить строку');
-      }
-  };
-  
-  const getUser3 = async () => {
-    try {
-    const firstNamePromise = getRandomStringWithRetries(5, 0.1);
-    const lastNamePromise = getRandomStringWithRetries(5, 0.1);
+};
+
+const getUser3 = async () => {
+  try {
+    const firstNamePromise = getRandomStringWithRetries(5, 0.7); // Имя с вероятностью успеха 0.7
+    const lastNamePromise = getRandomStringWithRetries(5, 0.9); // Фамилия с вероятностью успеха 0.9
+
     const [firstName, lastName] = await Promise.all([firstNamePromise, lastNamePromise]);
     return `${lastName} ${firstName}`; 
   } catch (error) {
@@ -26,6 +30,9 @@ function randomStringWithChance(successRate = 0.7) {
     return 'Не удалось получить имя пользователя';
   }
 };
+
+console.log(new Date());
+getUser3().then(user => {
+  console.log(user);
   console.log(new Date());
-  console.log(await getUser3());
-  console.log(new Date());
+});
